d9b673a2ec0c1ce13e5ceba1376f954965aedb33
Keely001 <thindiuke@gmail.com>
2023-07-17 09:04:27 +0300

    Basic functionallity

--- taskmate_m/src/components/ShowTask.js
+++ taskmate_m/src/components/ShowTask.js
@@ -1,5 +1,27 @@
-export const ShowTask = () => {
+export const ShowTask = ({tasklist, setTasklist}) => {
+
   return (
-    <div>ShowTask</div>
+    <section className="showTask">
+        <div className="head">
+            <div>
+                <span className="title">Todo</span>
+                <span className="count">{tasklist.length}</span>
+            </div> 
+            <button className="clearAll" onClick={() => (setTasklist([]))}>Clear All</button>
+        </div>
+        <ul>
+            {tasklist.map((task) => {
+            return <li key={task.id}>
+                <p>
+                    <span>{task.name}</span>
+                    <span>{task.time}</span>
+                </p>
+                <i className="bi bi-pencil-square"></i>
+                <i className="bi bi-trash"></i>
+            </li>
+            })}
+        </ul>
+
+    </section>
   )
 }
