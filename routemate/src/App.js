e2152d46e88243f85273994c387bd291550034d5
Keely001 <thindiuke@gmail.com>
2023-07-18 21:42:46 +0300

    new project

---
+++ routemate/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Home } from './components/Home';
+import { Footer } from './components/Footer';
+import { Contact } from './components/Contact';
+import { ProductDetail } from './components/ProductDetail';
+import { ProductList } from './components/ProductList';
+
+
+
+function App() {
+  
+  return (
+    <div className ='App'>
+    
+    </div>
+  );
+}
+
+export default App;
